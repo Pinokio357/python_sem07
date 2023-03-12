9241617ed385436d8c8e9c9bf1e9331c34210bfd
ivanH <nigga.2023@mail.ru>
2023-03-12 16:53:14 +0300

    zadacha 2

---
+++ ex2.py
@@ -0,0 +1,12 @@
+﻿num_rows = 6
+num_colums = 6
+operation = lambda x,y: x * y
+
+def print_operation_table(function, r, c):
+    for row in range(1, r + 1):
+        for colum in range(1, c + 1):
+            print(function(row, colum), end="\t")
+
+        print()
+
+print_operation_table(operation, num_rows, num_colums)
